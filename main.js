afd0330b5b1ac58323a4aeca7dddd6d9dad4221a
Duyen Nguyen <hongduyen0705@gmail.com>
2022-03-19 23:58:29 +0200

    rewrote fetch function and render function

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
 // Variables
 const cards = document.querySelector(".cards");
 
-const clearButton = document.querySelector(".clear-btn");
+const clearButton = document.querySelector(".clear-button");
 
 // Render card
-const renderCard = function (jobAd) {
-  let newButton = "";
-  if (jobAd.new == true) {
-    newButton += `<button class="new-btn">New</button>`;
-  } else {
-    newButton = "";
-  }
+const renderCard = function (array) {
+  array.forEach((jobAd) => {
+    let newButton = "";
+    if (jobAd.new == true) {
+      newButton += `<button class="new-btn">New</button>`;
+    } else {
+      newButton = "";
+    }
 
-  let featureBtn = "";
-  if (jobAd.featured == true) {
-    featureBtn += `<button class="feature-btn">feature</button>`;
-  } else {
-    featureBtn = "";
-  }
+    let featureBtn = "";
+    if (jobAd.featured == true) {
+      featureBtn += `<button class="feature-btn">feature</button>`;
+    } else {
+      featureBtn = "";
+    }
 
-  let languages = "";
-  jobAd.languages.forEach((lang) => {
-    languages += `<button class="language">${lang}</button>`;
+    let languages = "";
+    jobAd.languages.forEach((lang) => {
+      languages += `<button class="language">${lang}</button>`;
+    });
+
+    let tools = "";
+    jobAd.tools.forEach((tool) => {
+      tools += `<button class="tool">${tool}</button>`;
+    });
+
+    let html = `
+        <div class="card">
+                 
+        <div class="item-start">
+          <img class="logo" src="${jobAd.logo}" alt="" />
+      
+          <div class="item-start-content">
+            <span class="company">${jobAd.company}</span>
+           ${newButton}
+           ${featureBtn}
+            <h3 class="position">${jobAd.position}</h3>
+            <span class="postedAt">${jobAd.postedAt}</span> .
+            <span class="contract">${jobAd.contract}</span> .
+            <span class="location">${jobAd.location}</span>
+          </div>
+        </div>
+      
+        <div class="item-end">
+        
+          <button class="role">${jobAd.role}</button>
+       
+          <button class="level">${jobAd.level}</button>
+         
+          ${languages}
+          ${tools}
+        </div>
+      </div>
+        `;
+    cards.insertAdjacentHTML("beforeend", html);
   });
-
-  let tools = "";
-  jobAd.tools.forEach((tool) => {
-    tools += `<button class="tool">${tool}</button>`;
-  });
-
-  let html = `
-  <div class="card">
-           
-  <div class="item-start">
-    <img class="logo" src="${jobAd.logo}" alt="" />
-
-    <div class="item-start-content">
-      <span class="company">${jobAd.company}</span>
-     ${newButton}
-     ${featureBtn}
-      <h3 class="position">${jobAd.position}</h3>
-      <span class="postedAt">${jobAd.postedAt}</span> .
-      <span class="contract">${jobAd.contract}</span> .
-      <span class="location">${jobAd.location}</span>
-    </div>
-  </div>
-
-  <div class="item-end">
-  
-    <button class="role">${jobAd.role}</button>
- 
-    <button class="level">${jobAd.level}</button>
-   
-    ${languages}
-    ${tools}
-  </div>
-</div>
-  `;
-  cards.insertAdjacentHTML("beforeend", html);
 };
 
 // fetch("./data.json")
@@ -68,12 +70,17 @@ const renderCard = function (jobAd) {
 //     });
 //   });
 
+let jobsArray = [];
 async function fetchJobs() {
   let response = await fetch("./data.json");
   let data = await response.json();
-  data.forEach((item) => {
-    renderCard(item);
-  });
+  jobsArray = data;
+  console.log("jobsArray: ", jobsArray);
+  renderCard(jobsArray);
 }
 
 fetchJobs();
+
+clearButton.addEventListener("click", function () {
+  renderCard(jobsArray);
+});
